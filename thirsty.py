8d874697508dbb635a0883ce9a6b1c20637fcf85
sandravarghese23 <sandra.varghese@aspirenxt.com>
2021-06-21 13:00:10 +0530

    eliminated

--- thirsty.py
+++ thirsty.py
@@ -2,13 +2,12 @@ thirsty=input("Are you Thirsty")
 if thirsty=="yes":
     print("drink water")
     print("drink cola")
-<<<<<<< HEAD
     print("drink soda")
-=======
     print("drink juice")
->>>>>>> 005e58a020c3c688ed0f4f3f7e367fb2143a1373
+
 else :
     hungry=input("Are you Hungry")
 if thirsty=="yes":
     print("eat fruits")
+   
    
